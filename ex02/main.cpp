63337d0bf26d68118a77f7ca7b8226f4a5d0331f
Saliinger <alexisnoukan@gmail.com>
2025-04-20 18:03:09 +0200

    done

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,29 +1,30 @@
 #include "./include/ClapTrap.hpp"
 #include "./include/ScavTrap.hpp"
+#include "./include/FragTrap.hpp"
 
 void test_name() {
     std::cout << "Name test" << std::endl;
-    ScavTrap scav("Scavvy");
-    scav.show();
+    FragTrap frag("fraggy");
+    frag.show();
     std::cout << "end name test" << std::endl;
 }
 
 void classique_test() {
     std::cout << "Classique test" << std::endl;
-    ScavTrap scav;
-    scav.show();
-    scav.attack("target");
-    scav.takeDamage(5);
-    scav.beRepaired(3);
-    scav.show();
+    FragTrap frag;
+        frag.show();
+    frag.attack("target");
+    frag.takeDamage(5);
+    frag.beRepaired(3);
+    frag.show();
     std::cout << "end classique test" << std::endl;
 }
 
 void test_copy() {
     std::cout << "Copy test" << std::endl;
-    ScavTrap scav1("Scavvy");
-    ScavTrap scav2(scav1);
-    scav2.show();
+    FragTrap frag1("fragvy");
+    FragTrap frag2(frag1);
+    Frag2.show();
     std::cout << "end copy test" << std::endl;
 }
 
